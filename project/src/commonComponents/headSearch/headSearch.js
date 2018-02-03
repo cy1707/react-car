322b7e79e4e4d7b8e55db5e3868ff0d2e5466dfd
cy1707 <337502665@qq.com>
2018-02-03 18:02:01 +0800

    顶部和底部

--- project/src/commonComponents/headSearch/headSearch.js
+++ project/src/commonComponents/headSearch/headSearch.js
@@ -1,12 +1,27 @@
 import React, {Component} from 'react'
 
+import './headSearch.scss'
+
 export default class HeadSearchComponent extends Component{
     render(){
         return (
-            <div> 
-                <div>HeadSearchComponent</div>
-                <h3>头部搜索</h3>
-                <footList/>
+            <div className="headListc"> 
+                <ul>
+                    <li className = "headListc_l">
+                        <span className = "currentCity">广州</span>
+                        <i className="angle down icon"></i>
+                    </li>
+                    <li className = "headListc_c">
+                        <div className="ui disabled icon input">
+                            <i className="search icon"></i>
+                            <input type="text" placeholder="搜索您想要的车" />
+                        </div>
+                    </li>
+                    <li className = "headListc_r">
+                        <i className="comment icon"></i>
+                    </li>
+                </ul>
+                
             </div>
         )
     }
--- project/src/components/carlist/carList.js
+++ project/src/components/carlist/carList.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react'
 
+
+import FootListComponent from '../../commonComponents/footList/footList'
+
 export default class CarListComponent extends Component{
     render(){
         return (
             <div> 
                 <div>CarListComponent</div>
                 <h3>列表页</h3>
+               
+                <FootListComponent/>
+                
+                
             </div>
         )
     }
